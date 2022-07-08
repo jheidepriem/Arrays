a91b31a3d2a044a93595e4f1eac5751f3f564332
Josephine Heidepriem <jheidepriem@gmail.com>
2022-07-08 16:20:04 -0500

    Add array methods and explanations

--- arrays.js
+++ arrays.js
@@ -1,3 +1,9 @@
 var colors = ["blue", "yellow", "green", "red"];
+//pop removes the last element of an array
+colors.pop();
 var number = ["1", "2", "3", "4"];
+//gets rid of the element in that index position
+number.splice(1, 3);
 var status = ["paid", "unpaid","null","void"];
+//brings back the element in the last position
+status.push()
